db9547c4b42c05590b3dd89727e9a257985d9666
Miguel Mendoza <miguelmendoza@google.com>
2024-04-14 21:03:54 -0700

    feat: add support for --set-graphql

---
+++ pkg/flags/setgraphql.go
@@ -0,0 +1,55 @@
+//  Copyright 2024 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package flags
+
+import (
+	"fmt"
+	"github.com/go-errors/errors"
+	"github.com/micovery/apigee-yaml-toolkit/pkg/parser"
+	"github.com/micovery/apigee-yaml-toolkit/pkg/values"
+	"strings"
+)
+
+type SetGraphQL struct {
+	Data *values.Map
+}
+
+func NewSetGraphQL(data *values.Map) SetGraphQL {
+	return SetGraphQL{Data: data}
+}
+
+func (v *SetGraphQL) String() string {
+	return fmt.Sprintf("%v", v.Data)
+}
+
+func (v *SetGraphQL) Set(entry string) error {
+	key, filePath, found := strings.Cut(entry, "=")
+	if !found {
+		return errors.Errorf("missing file path in set-graphql for key=%s", key)
+	}
+
+	parserResult, schemaBytes, err := parser.ParseGraphQLSchema(filePath)
+	if err != nil {
+		return err
+	}
+
+	schema := *parserResult
+	schemaFileText := string(schemaBytes)
+
+	v.Data.Set(key, schema)
+	v.Data.Set(fmt.Sprintf("%s_string", key), schemaFileText)
+
+	return nil
+}
