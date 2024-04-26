da39abe19ee2c006ca1a25b30b9b4914402e908f
Miguel Mendoza <miguelmendoza@google.com>
2024-04-26 00:02:49 -0700

    fix: resolve YAML refs immediately after rendering template
    When rendering an apiproxy or sharedflow, resolve the YAML refs
    immediately after rendering the input template. This makes it
    so that you don't need to manually copy the files in the $refs

--- pkg/render/model.go
+++ pkg/render/model.go
@@ -15,10 +15,12 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/go-errors/errors"
 	"github.com/micovery/apigee-go-gen/pkg/apigee/v1"
 	"github.com/micovery/apigee-go-gen/pkg/flags"
+	"github.com/micovery/apigee-go-gen/pkg/utils"
 	"os"
 	"path/filepath"
 )
@@ -37,6 +39,24 @@ func GenerateBundle(createModelFunc func(string) (v1.Model, error), cFlags *Comm
 	// render the template to a temporary location
 	cFlags.OutputFile = flags.String(filepath.Join(tmpDir, "model.yaml"))
 	err = RenderGenericTemplate(cFlags, false)
+	if err != nil {
+		return errors.New(err)
+	}
+
+	templateFile := string(cFlags.TemplateFile)
+
+	//resolve YAML Refs immediately after rendering to avoid having copy files in $ref
+	rendered, err := os.ReadFile(string(cFlags.OutputFile))
+	if err != nil {
+		return errors.New(err)
+	}
+
+	rendered, err = ResolveYAML(rendered, templateFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(string(cFlags.OutputFile), rendered, os.ModePerm)
 	if err != nil {
 		return errors.New(err)
 	}
@@ -85,4 +105,28 @@ func CreateBundle(model v1.Model, output string, validate bool, dryRun string) (
 	}
 
 	return nil
+}
+
+func ResolveYAML(text []byte, filePath string) ([]byte, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, errors.New(err)
+	}
+	defer func() { utils.Must(os.Chdir(wd)) }()
+
+	err = os.Chdir(filepath.Dir(filePath))
+	if err != nil {
+		return nil, errors.New(err)
+	}
+
+	yaml, err := utils.Text2YAML(bytes.NewReader(text))
+	if err != nil {
+		return nil, err
+	}
+	newText, err := utils.YAML2Text(yaml, 2)
+	if err != nil {
+		return nil, err
+	}
+
+	return newText, nil
 }
